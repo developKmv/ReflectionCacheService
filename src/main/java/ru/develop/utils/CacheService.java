62091c13ec313b44874434581200de9340238fa1
mvx <maxrprp@gmail.com>
2024-03-11 10:56:56 +0300

    solution 3

--- src/main/java/ru/develop/utils/CacheService.java
+++ src/main/java/ru/develop/utils/CacheService.java
@@ -5,7 +5,9 @@ import ru.develop.entity.Fractionable;
 import ru.develop.my.annotations.Cache;
 import ru.develop.my.annotations.Mutator;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
+import java.util.HashMap;
 
 
 @Slf4j
@@ -13,11 +15,23 @@ public class CacheService<T> implements InvocationHandler {
 
     private T o;
     private Double cacheValue;
+    private static HashMap<String,String> methodsInfo = new HashMap<>();
     public CacheService(T o){
         this.o = o;
     };
 
     public static <T> T cache(T in){
+        Class<?> inClass = in.getClass();
+        Method[] methods = inClass.getMethods();
+        for(Method m: methods){
+            if (m.isAnnotationPresent(Cache.class)){
+                methodsInfo.put(m.getName(), "Cache");
+            }else if(m.isAnnotationPresent(Mutator.class)){
+                methodsInfo.put(m.getName(), "Mutator");
+            }
+
+        }
+
         ClassLoader classLoader = in.getClass().getClassLoader();
         Class[] interfaces = in.getClass().getInterfaces();
         T out = (T) Proxy.newProxyInstance(classLoader,interfaces,new CacheService<>(in));
@@ -26,14 +40,14 @@ public class CacheService<T> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if(method.isAnnotationPresent(Cache.class) && cacheValue ==null){
+        if(methodsInfo.containsKey(method.getName()) && methodsInfo.get(method.getName()) == "Cache" &&cacheValue == null){
             return cacheValue = (Double) method.invoke((Fractionable)o,args);
-        }else if (method.isAnnotationPresent(Cache.class) && cacheValue !=null){
+        }else if (methodsInfo.containsKey(method.getName()) && methodsInfo.get(method.getName()) == "Cache"  && cacheValue !=null){
             return cacheValue;
         }
 
 
-        if(method.isAnnotationPresent(Mutator.class)){
+        if(methodsInfo.containsKey(method.getName()) && methodsInfo.get(method.getName()) == "Mutator"){
             cacheValue = null;
         }
 
